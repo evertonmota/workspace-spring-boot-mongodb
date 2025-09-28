c54ffbe8bd684d74fd746fa113f58d7338efc5aa
Everton Mota <mcostagt@gmail.com>
2025-09-27 22:05:28 -0300

    Usando padrao DTO no Controller para persitir Users

--- src/main/java/br/com/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/workshopmongo/resources/UserResource.java
@@ -1,29 +1,29 @@
 package br.com.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.workshopmongo.domain.User;
+import br.com.workshopmongo.dto.UserDTO;
 import br.com.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
 public class UserResource {
 	
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	@GetMapping(value = "/users")
+	public ResponseEntity<List<UserDTO>> findAll(){
 
 		List<User> users = userService.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> dtos = users.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dtos);
 	}
 }
